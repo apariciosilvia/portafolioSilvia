c473e5358b7de0912579a808d5413305a9848858
apariciosilvia <silvia.aparicio.martin@gmail.com>
2025-01-30 12:07:49 +0100

    Delete AMS-Portfolio directory

--- AMS-Portfolio/js/lectura.js
+++
@@ -1,6 +0,0 @@
-function leerTexto() {
-    let texto = document.body.innerText; // Obtiene el texto visible de la página
-    let voz = new SpeechSynthesisUtterance(texto);
-    voz.lang = 'es-ES'; // Establece el idioma español
-    window.speechSynthesis.speak(voz);
-}
